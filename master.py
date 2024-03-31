0cb7666fe85af6f14b3e8d228353096641d74784
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-03-31 17:05:00 +0530

    initial commit

---
+++ master.py
@@ -0,0 +1,56 @@
+import subprocess
+import time
+import main1
+import main2
+
+
+
+def algo():
+    car_count1, people_count1 = main1.run()
+    car_count2, people_count2 = main2.run()
+
+    print(car_count1)
+    print(car_count2)
+
+    print(people_count1)
+    print(people_count2)
+
+
+    final_count = 0
+    if car_count1 and car_count2 != 0:
+        ratio = car_count1/car_count2
+
+        if ratio > 1.5 and people_count1 < 7:
+            green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
+            final_count = car_count1 * 7
+        elif ratio > 1.5 and people_count1 > 7:
+            green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
+            final_count = car_count2 * 7
+        elif ratio < 1.5 and people_count2 > 7:
+            green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
+            final_count = car_count2 * 7
+        else:
+            green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
+            final_count = car_count2 * 7
+
+    # Determine which subprocess gets the green signal
+    else:
+        if car_count1 > car_count2:
+            green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
+            final_count = car_count1 * 7
+        elif car_count1 < car_count2:
+            green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
+            final_count = car_count2 * 7
+        else:
+            green_signal = "same number of cars"
+            final_count = 30
+    # Print the signals
+    print(green_signal)
+
+    return final_count
+
+while(True):
+    final_count = algo()
+    start_time = time.time()
+    time.sleep(final_count)
+    end_time = time.time()
