c8ab774b6a7852d291ea8750b7832525510aaa51
siliconvolley <rishabhlaxmikanth@gmail.com>
2024-09-29 15:54:39 +0530

    minor change

--- master.py
+++ master.py
@@ -3,7 +3,7 @@ import time
 import main1
 import main2
 
-
+CAR_TRAVEL_TIME: int = 7
 
 def algo():
     car_count1, people_count1 = main1.run()
@@ -19,28 +19,28 @@ def algo():
     final_count = 0
     if car_count1 and car_count2 != 0:
         ratio = car_count1/car_count2
-
+        
         if ratio > 1.5 and people_count1 < 7:
             green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
-            final_count = car_count1 * 7
+            final_count = car_count1 * CAR_TRAVEL_TIME
         elif ratio > 1.5 and people_count1 > 7:
             green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
-            final_count = car_count2 * 7
+            final_count = car_count2 * CAR_TRAVEL_TIME
         elif ratio < 1.5 and people_count2 > 7:
             green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
-            final_count = car_count2 * 7
+            final_count = car_count2 * CAR_TRAVEL_TIME
         else:
             green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
-            final_count = car_count2 * 7
+            final_count = car_count2 * CAR_TRAVEL_TIME
 
     # Determine which subprocess gets the green signal
     else:
         if car_count1 > car_count2:
             green_signal = "Signal-1 => Green Signal, Signal-2 => Red Signal"
-            final_count = car_count1 * 7
+            final_count = car_count1 * CAR_TRAVEL_TIME
         elif car_count1 < car_count2:
             green_signal = "Signal-2 => Green Signal, Signal-1 => Red Signal"
-            final_count = car_count2 * 7
+            final_count = car_count2 * CAR_TRAVEL_TIME
         else:
             green_signal = "same number of cars"
             final_count = 30
